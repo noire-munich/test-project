86deb51855bcd35afa35c46e04b10facb69185d4
Thomas Seillan <thomas.seillan@gmail.com>
2023-03-31 19:20:08 +0200

    Update working pipeline.

--- scripts/variables.js
+++ scripts/variables.js
@@ -5,5 +5,6 @@ import { db } from 'api/src/lib/db'
 export default async ({ args }) => {
   // Your script here...
   console.log(':: Executing script with args ::')
-  console.log(process.env)
+  console.log('DATABASE_URL', process.env.DATABASE_URL)
+  console.log('SESSION_SECRET', process.env.SESSION_SECRET)
 }
